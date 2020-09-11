71d5477e4080438f66fa635ac1845c9ea7d406af
bashbashbash <7basch@gmail.com>
2020-09-11 15:17:25 -0400

    got summonerDetails function working

--- UserInfo.go
+++ UserInfo.go
@@ -7,9 +7,10 @@ import (
 	"net/http"
 )
 
-const apiKey string = "RGAPI-738ddbf9-45a4-452c-b26b-2bb6ee8fcade"
+const apiKey string = "RGAPI-6860c312-4a43-4556-b7ae-478a0291b64a"
 
 // UserInfo resonse body json
+// API SUMMONER-V4
 type UserInfo struct {
 	ID            string
 	AccountID     string
@@ -17,26 +18,65 @@ type UserInfo struct {
 	Name          string
 	ProfileIconID int
 	RevisionDate  int
-	SummerLevel   int
+	SummonerLevel int
+}
+
+// league entries in all queues for a given summoner ID response body json
+// LEAGUE-V4
+type LeagueEntries struct {
+	LeagueID     string
+	SummonerID   string
+	SummonerName string
+	QueueType    string
+	Tier         string
+	Rank         string
+	LeaguePoints int
+	Wins         int
+	Losses       int
+	HotStreak    bool
+	Veteran      bool
+	Freshblood   bool
+	Inactive     bool
 }
 
 func main() {
 	userInfo := summonerInfo("bashbashbash")
 
-	fmt.Printf("%s is the name of the user and the PUUID is: %s", userInfo.Name, userInfo.Puuid)
+	userInfoJSON, _ := json.MarshalIndent(userInfo, "", "  ")
+	fmt.Printf("MarshalIndent funnction output\n %s\n", string(userInfoJSON))
+
+	details := summonerDetails(userInfo.ID)
+	detailsJSON, _ := json.MarshalIndent(details, "", "  ")
+	fmt.Printf("MarshalIndent funnction output\n %s\n", string(detailsJSON))
 }
 
-func summonerInfo(summonerName string) UserInfo {
-	resp, err := http.Get("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName + "?api_key=" + apiKey)
+func summonerDetails(accountID string) LeagueEntries {
+	resp, err := http.Get("https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/1xcRetZtGjvYhsN45YxKY4jxRWxLvFXywfEJ7Vm0VzfFjBc?api_key=RGAPI-6860c312-4a43-4556-b7ae-478a0291b64a")
 	printIferr(err)
 
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	printIferr(err)
-	var userInfo UserInfo
 
+	var leagueEntries []LeagueEntries
+	err = json.Unmarshal(body, &leagueEntries)
+	printIferr(err)
+
+	return leagueEntries[0]
+}
+
+func summonerInfo(summonerName string) UserInfo {
+	resp, err := http.Get("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-account/PFo90JxR0-a7fttW-cSYNSoJQZ15uNQp-W44WlCMELwxeHA?api_key=RGAPI-6860c312-4a43-4556-b7ae-478a0291b64a")
+	printIferr(err)
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	printIferr(err)
+
+	var userInfo UserInfo
 	err = json.Unmarshal(body, &userInfo)
 	printIferr(err)
+
 	return userInfo
 }
 
